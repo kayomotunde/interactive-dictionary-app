da9782b32555ca5611c4e51a9767b6778f18354c
kayomotunde <kayomotunde@gmail.com>
2020-11-21 13:39:51 -0800

    optimized final output

--- app.py
+++ app.py
@@ -4,10 +4,16 @@ from difflib import get_close_matches
 # Load json data into python dictionary data
 data = json.load(open('data.json'))
 
+
 def translate(word):
+    
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     elif len(get_close_matches(word, data.keys())) > 0:
         word_other = get_close_matches(word, data.keys())[0]
         yn = input(f"Did you mean {word_other} instead? Enter Y if yes, or N if No: ").capitalize()
@@ -29,3 +35,4 @@ if type(output) == list:
         print(item)
 else:
     print(output)
+    
